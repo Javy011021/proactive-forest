295bbb6cd6aad206f608e0e1b2bf852485fc53de
Laura Gonzalez <laura.gonzalez@avangenio.com>
2025-04-17 10:34:36 -0400

    chore: get RF base values

--- classification_batch_cross_validate.py
+++ classification_batch_cross_validate.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     data = pd.DataFrame()
 
-    for name, loader in load_batch.get_batch_3():
+    for name, loader in load_batch.get_batch_4():
         saver = pd.DataFrame()
         data_name = name
         X, y = loader[0], loader[1]
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         start = time.time()
 
         recall, roc_auc, accracy, pcd, presi, incial_values, final_values = utils.cross_validation_train_with_pruning(
-            fc, train, test, pruning="window_threshold")
+            fc, train, test)
 
         end = time.time()
         duration = (end-start) / 60
